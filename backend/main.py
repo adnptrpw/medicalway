f098831e1611948831320fd2bd4eb4fa8a2ec153
adnptrpw <andini.putri02@ui.ac.id>
2022-12-09 13:49:16 +0700

    Change serp data

--- backend/main.py
+++ backend/main.py
@@ -20,12 +20,12 @@ def search(request):
     rank = BSBI.retrieve_bm25(query, k=100, k1=2.75, b=0.75)
     end_time = time.time()
 
-    serp = []
+    serp = {}
     serp_length = 0
 
     for (_, doc) in rank:
         serp_length += 1
         with bucket.blob(doc.replace("\\", "/")).open("r") as file:
-            serp.append(file.read())
+            serp[doc.replace("\\", "/")] = file.read()
 
     return {"duration": end_time - start_time, "serp length": serp_length, "serp": serp}, 200
